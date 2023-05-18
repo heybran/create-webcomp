37842334a9ad5bc8eec2750cf7578c66185338db
brandon <gwonzhang@gmail.com>
2023-05-18 21:00:06 +0800

    extract util methods into util.js file

--- src/index.js
+++ src/index.js
@@ -18,6 +18,17 @@ import {
   yellow,
 } from "kolorist";
 
+import {
+  formatTargetDir,
+  getProjectName,
+  isEmpty,
+  emptyDir,
+  copy,
+  isValidPackageName,
+  toValidPackageName,
+  pkgFromUserAgent,
+} from "./util.js";
+
 /**
  * @typeof {Object} Argv
  * @property {string} [t]
@@ -33,6 +44,33 @@ import {
  * @property {string[]} [_]
  */
 
+/**
+ * @typeof {Object} WebComponentsStructure
+ * @property {string} name
+ * @property {string} [display]
+ * @property {string}
+ */
+
+/** @type WebComponentsStructure **/
+const STRUCTURES = [
+  {
+    name: "js-only",
+    display: `${yellow("Standalone JavaScript")} ("./components/MyCouter.js")`,
+  },
+  {
+    name: "css-js",
+    display: `${magenta(
+      "CSS + JavaScript",
+    )} ("./components/my-counter/MyCouter.css+MyCounter.js")`,
+  },
+  {
+    name: "html-css-js",
+    display: `${green(
+      "HTML + CSS + JavaScript",
+    )} ("./components/my-counter/MyCouter.html+MyCounter.css+MyCouter.js")`,
+  },
+];
+
 /** @type {Argv} */
 const argv = minimist(process.argv.slice(2), { string: ["_"] });
 
@@ -42,9 +80,23 @@ const cwd = process.cwd();
 /** type {string} */
 const defaultTargetDir = "web-components-project";
 
+/**
+ * A record of file name mappings for renaming files during project initialization.
+ * @typedef {Object.<string, string|undefined>} RenameFiles
+ */
+
+/**
+ * A record of file name mappings for renaming files during project initialization.
+ * @type {RenameFiles}
+ */
+const renameFiles = {
+  _gitignore: ".gitignore",
+};
+
 async function init() {
   const argTargetDir = formatTargetDir(argv._[0]);
   const argTemplate = argv.template || argv.t;
+  const argStructure = argv.structure || argv.s;
 
   let targetDir = argTargetDir || defaultTargetDir;
   const projectName = getProjectName(targetDir);
@@ -79,65 +131,104 @@ async function init() {
           );
         },
       },
+      {
+        type: () =>
+          isValidPackageName(getProjectName(targetDir)) ? null : "text",
+        name: "packageName",
+        message: reset("Package name:"),
+        initial: () => toValidPackageName(getProjectName(targetDir)),
+        validate: (dir) =>
+          isValidPackageName(dir) || "Invalid package.json name",
+      },
+      {
+        type: argStructure ? null : "select",
+        name: "structure",
+        message: reset("Select a web components structure:"),
+        initial: 0,
+        choices: STRUCTURES.map((structure) => {
+          return {
+            title: structure.display,
+            value: structure,
+          };
+        }),
+      },
     ]);
   } catch (err) {
     console.log(err.message);
     return;
   }
 
-  const { overwrite } = result;
+  const { overwrite, packageName, structure } = result;
   console.log(result);
 
   const root = path.join(cwd, targetDir);
 
   if (overwrite) {
     emptyDir(root);
+  } else if (!fs.existsSync(root)) {
+    fs.mkdirSync(root, { recursive: true });
   }
-}
 
-/**
- * Formats a target directory path by trimming trailing slashes.
- * @param {string|undefined} targetDir - The target directory path to format.
- * @returns {string|undefined} The formatted target directory path, or undefined
- */
-function formatTargetDir(targetDir) {
-  return targetDir?.trim().replace(/\/+$/g, "");
-}
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  const pkgManager = pkgInfo ? pkgInfo.name : "npm";
+  const isYarn1 = pkgManager === "yarn" && pkgInfo?.version.startsWith("1.");
 
-/**
- * Get project name from target directory path.
- * @param {string} targetDir - The target directory path.
- * @returns {string} The project name.
- */
-function getProjectName(targetDir) {
-  return targetDir === "." ? path.basename(path.resolve()) : targetDir;
-}
+  console.log(`\nScaffolding web components project in ${root}...`);
 
-/**
- * Checks if a directory is empty.
- * @param {string} path - The path to the directory to check.
- * @returns {boolean} True if the directory is empty, false otherwise.
- */
-function isEmpty(path) {
-  const files = fs.readdirSync(path);
-  return files.length === 0 || (files.length === 1 && files[0] === ".git");
-}
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    "../..",
+    `template-${structure.name}`,
+  );
 
-/**
- * Deletes all files and directories in a directory, except for the .git directory.
- * @param {string} dir - The path to the directory to empty.
- * @returns {void}
- */
-function emptyDir(dir) {
-  if (!fs.existsSync(dir)) {
-    return;
-  }
-  for (const file of fs.readdirSync(dir)) {
-    if (file === ".git") {
-      continue;
+  /**
+   * Writes content to a file, or copies a file from a template directory to a target directory.
+   * @param {string} file - The name of the file to write or copy.
+   * @param {string} [content] - The content to write to the file. If not provided, the file will be copied from a template directory.
+   * @returns {void}
+   */
+  const write = (file, content) => {
+    const targetPath = path.join(root, renameFiles[file] ?? file);
+    if (content) {
+      fs.writeFileSync(targetPath, content);
+    } else {
+      copy(path.join(templateDir, file), targetPath);
     }
-    fs.rmSync(path.resolve(dir, file), { recursive: true, force: true });
+  };
+
+  // template files that we need to copy over target directory.
+  const files = fs.readdirSync(templateDir);
+  for (const file of files.filter((f) => f !== "package.json")) {
+    write(file);
   }
+
+  const pkg = JSON.parse(
+    fs.readFileSync(path.join(templateDir, `package.json`), "utf-8"),
+  );
+
+  pkg.name = packageName || getProjectName(targetDir);
+  write("package.json", JSON.stringify(pkg, null, 2) + "\n");
+
+  const cdProjectName = path.relative(cwd, root);
+  if (root !== cwd) {
+    console.log(
+      `  cd ${
+        cdProjectName.includes(" ") ? `"${cdProjectName}"` : cdProjectName
+      }`,
+    );
+  }
+
+  switch (pkgManager) {
+    case "yarn":
+      console.log("  yarn");
+      console.log("  yarn dev");
+      break;
+    default:
+      console.log(`  ${pkgManager} install`);
+      console.log(`  ${pkgManager} run dev`);
+      break;
+  }
+  console.log();
 }
 
 init().catch((e) => console.error(e));
