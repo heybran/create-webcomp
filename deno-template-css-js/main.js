8ed4f5a4186b90c31c984f5c447dc6112213ec9d
brandon <gwonzhang@gmail.com>
2023-05-23 20:22:23 +0800

    Revert "templates: add 3 templates for "Deno""
    This reverts commit 63ddc80346cc06838a6f53fd798efeb22ee219c1.

--- deno-template-css-js/main.js
+++
@@ -1,4 +0,0 @@
-import "./style.css";
-import "./src/components/my-app/index.js";
-
-document.querySelector("#app").innerHTML = `<my-app></my-app>`;
--- deno-template-js-only/main.js
+++
@@ -1,4 +0,0 @@
-import "./style.css";
-import "./src/components/MyApp.js";
-
-document.querySelector("#app").innerHTML = `<my-app></my-app>`;
